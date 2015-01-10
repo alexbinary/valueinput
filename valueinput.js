92184188408edc650bf29c65ea63af88c23347e5
alex <alexandre.bintz@gmail.com>
2015-01-10 11:11:09 +0100

    DOM tree initialization

--- valueinput.js
+++ valueinput.js
@@ -6,3 +6,63 @@
  */
 
 "use strict";
+
+/**
+ * ValueInput
+ *
+ * constructor
+ */
+function ValueInput(e) {
+
+  this.input = e;
+  this.input.type = 'hidden';
+
+  this.wrapper = document.createElement('span');
+
+  this.select = document.createElement('select');
+  var optElement = document.createElement('option');
+  optElement.value = 'string';
+  optElement.label = 'string';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'number';
+  optElement.label = 'number';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'boolean';
+  optElement.label = 'boolean';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'array';
+  optElement.label = 'array';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'object';
+  optElement.label = 'object';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'null';
+  optElement.label = 'null';
+  this.select.add(optElement);
+  var optElement = document.createElement('option');
+  optElement.value = 'undefined';
+  optElement.label = 'undefined';
+  this.select.add(optElement);
+
+  this.stringInput = document.createElement('input');
+  this.stringInput.type = 'text';
+
+  this.numberInput = document.createElement('input');
+  this.numberInput.type = 'number';
+
+  this.arrayInputs = [];
+
+  this.objectLabelInputs = [];
+  this.objectValueInputs = [];
+
+  this.input.parentElement.replaceChild(this.wrapper, this.input);
+  this.wrapper.appendChild(this.select);
+  this.wrapper.appendChild(this.input);
+
+  this.select.addEventListener('change', this.onSelectChange);
+}

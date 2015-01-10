9bf599db9c41707cb7b7bc10ee8abab4c337642c
alex <alexandre.bintz@gmail.com>
2015-01-10 11:54:53 +0100

    String and Number support

--- valueinput.js
+++ valueinput.js
@@ -15,7 +15,6 @@
 function ValueInput(e) {
 
   this.input = e;
-  this.input.type = 'hidden';
 
   this.wrapper = document.createElement('span');
 
@@ -60,9 +59,101 @@ function ValueInput(e) {
   this.objectLabelInputs = [];
   this.objectValueInputs = [];
 
+  this.value = null;
+  this.previousSelectChoice = null;
+
   this.input.parentElement.replaceChild(this.wrapper, this.input);
   this.wrapper.appendChild(this.select);
-  this.wrapper.appendChild(this.input);
 
-  this.select.addEventListener('change', this.onSelectChange);
+  this.select.addEventListener('change', this.onSelectChange.bind(this));
+
+  this.stringInput.addEventListener('input', this.updateValue.bind(this));
+  this.numberInput.addEventListener('input', this.updateValue.bind(this));
+
+  this.onSelectChange();
+  this.updateValue();
+}
+
+ValueInput.prototype.onSelectChange = function(pEvent) {
+
+  this.unsetInput(this.previousSelectChoice);
+
+  var newSelectChoice = this.select.value;
+
+  this.setupInput(newSelectChoice);
+
+  this.previousSelectChoice = newSelectChoice;
+}
+
+ValueInput.prototype.unsetInput = function(pChoice) {
+
+  if(pChoice == 'string') {
+
+    this.wrapper.removeChild(this.stringInput);
+
+  } else if(pChoice == 'number') {
+
+    this.wrapper.removeChild(this.numberInput);
+
+  } else if(pChoice == 'boolean') {
+
+  } else if(pChoice == 'array') {
+
+  } else if(pChoice == 'object') {
+
+  } else if(pChoice == 'null') {
+
+  } else if(pChoice == 'undefined') {
+
+  }
+}
+
+ValueInput.prototype.setupInput = function(pChoice) {
+
+  if(pChoice == 'string') {
+
+    this.wrapper.appendChild(this.stringInput);
+
+  } else if(pChoice == 'number') {
+
+    this.wrapper.appendChild(this.numberInput);
+
+  } else if(pChoice == 'boolean') {
+
+  } else if(pChoice == 'array') {
+
+  } else if(pChoice == 'object') {
+
+  } else if(pChoice == 'null') {
+
+  } else if(pChoice == 'undefined') {
+
+  }
+}
+
+ValueInput.prototype.updateValue = function() {
+
+  var value = null;
+
+  if(this.select.value == 'string') {
+
+    value = String(this.stringInput.value);
+
+  } else if(this.select.value == 'number') {
+
+    value = Number(this.numberInput.value);
+
+  } else if(this.select.value == 'boolean') {
+
+  } else if(this.select.value == 'array') {
+
+  } else if(this.select.value == 'object') {
+
+  } else if(this.select.value == 'null') {
+
+  } else if(this.select.value == 'undefined') {
+
+  }
+
+  this.value = value;
 }
